f725e78c30461e71a6b88d7a8ec9b072f2dd987f
rubeniogames <ruben.label.27@gmail.com>
2024-05-30 18:41:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const playButton = document.getElementById('play-button');
     const settingsButton = document.getElementById('settings-button');
     const walletButton = document.getElementById('wallet-button');
+    const backButton = document.getElementById('back-button');
     const welcomeScreen = document.getElementById('welcome-screen');
     const mainMenu = document.getElementById('main-menu');
+    const underConstruction = document.getElementById('under-construction');
 
     startButton.addEventListener('click', function() {
         const username = document.getElementById('username').value;
@@ -16,9 +18,20 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    playButton.addEventListener('click', showUnderConstruction);
+    settingsButton.addEventListener('click', showUnderConstruction);
+    walletButton.addEventListener('click', showUnderConstruction);
+    backButton.addEventListener('click', showMainMenu);
+
     function showMainMenu() {
         welcomeScreen.classList.add('hidden');
+        underConstruction.classList.add('hidden');
         mainMenu.classList.remove('hidden');
+    }
+
+    function showUnderConstruction() {
+        mainMenu.classList.add('hidden');
+        underConstruction.classList.remove('hidden');
     }
 
     function init() {
